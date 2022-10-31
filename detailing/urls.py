1a253b3fc674c2f147479d37e33549b3c66d63fc
Kirill <77398659+steleur@users.noreply.github.com>
2022-11-01 02:56:59 +0300

    add files

---
+++ detailing/urls.py
@@ -0,0 +1,36 @@
+"""detailing URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+
+from django.conf.urls.static import static
+from web_site_app import settings
+from django.urls import path, include
+from web_site_app import views
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('services/', views.services, name='services'),
+    path('gallery', views.gallery, name='gallery'),
+    path('gallery/<slug:slug>', views.gallery_page, name='gallery_page'),
+    path('certificates', views.certificates, name='certificates'),
+    path('contacts', views.contacts, name='contacts')
+
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
